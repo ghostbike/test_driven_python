2a1b0ed6f76beef644cf98b84704724ced6a4e71
KRYS MATHIS <krysmathis@gmail.com>
2014-11-09 08:12:23 -0600

    Second commit

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,48 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase): #1
 
-assert 'Django' in browser.title
+	def setUp(self): #2
+		self.browser = webdriver.Firefox()
+
+	def tearDown(self): #3
+		self.browser.quit()
+
+	def test_can_start_a_list_and_retrieve_it_later(self):
+
+		# Edith has heard about a cool new online to-do app. She goes
+		# to check out it's homepage
+
+		self.browser.get('http://localhost:8000')
+		self.browser.implicitly_wait(3)
+
+		# she notices the page title and header mention to-do lists
+		self.assertIn('To-Do', self.browser.title) #5
+		self.fail('Finish the test!') #6
+
+		# She is invited to enter a to-do item straight away
+
+		# She types "buy peacock feathers" into a text box (Edith's hobby
+		# is tying fly-fishing lures)
+
+		# When she hits enter, the page updates and now the page lists
+		# "1: Buy peacock feathers" as an item in a to-do lists
+
+		# There is still a text box inviting her to add another item. She
+		# enters "Use peacock feathers to make a fly" (Edith is very methodical)
+
+		# The page updates again, and now both items on her lists
+
+		# Edith wonders whether the site will remember her list. Then she sees
+		# that the site has generated a uniqure URL for her -- there is some
+		# explanatory text to that effect.
+
+		# She visits that URL - her to-do list is still there.
+
+		# Satisfied, she goes back to sleep
+
+		browser.quit()
+if __name__ == '__main__':
+	unittest.main(warnings='ignore')
+
