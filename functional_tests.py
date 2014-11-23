2658a6df36182255700083727ed2e533ba7af0ad
KRYS MATHIS <krysmathis@gmail.com>
2014-11-23 09:15:55 -0600

    Prior to refactoring

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import unittest
+import time
 
 class NewVisitorTest(unittest.TestCase): #1
 
@@ -34,18 +35,37 @@ class NewVisitorTest(unittest.TestCase): #1
 		# When she hits enter, the page updates and now the page lists
 		# "1: Buy peacock feathers" as an item in a to-do lists
 		inputbox.send_keys(Keys.ENTER)
-
+		
+		
+		#time.sleep(10)
 		table = self.browser.find_element_by_id('id_list_table')
 		rows = table.find_elements_by_tag_name('tr')
-		self.assertTrue(any(row.text == '1: Buy peacock feathers' for row in rows),"New to-do item did not appear in table")
+		self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+		#self.assertIn('2: Buy peacock feathers to make a fly', [row.text for row in rows])
+		
+		time.sleep(3)
+		
 		# There is still a text box inviting her to add another item. She
 		# enters "Use peacock feathers to make a fly" (Edith is very methodical)
-		self.fail('Finish the test!')
+		table = self.browser.find_element_by_id('id_list_table')
+		
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		inputbox.send_keys('Use peacock feathers to make a fly')
+		inputbox.send_keys(Keys.ENTER)
+		
+		time.sleep(5)
+
 		# The page updates again, and now both items on her lists
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+		self.assertIn('2: Buy peacock feathers to make a fly', [row.text for row in rows])
+		
 
 		# Edith wonders whether the site will remember her list. Then she sees
 		# that the site has generated a uniqure URL for her -- there is some
 		# explanatory text to that effect.
+		self.fail('Finish the test!')
 
 		# She visits that URL - her to-do list is still there.
 
